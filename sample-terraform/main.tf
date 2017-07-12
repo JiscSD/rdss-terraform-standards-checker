00e0a12219ca1252dc6cd1f16dead5be557e185b
Mark Winterbottom <mark@londonappdeveloper.com>
2017-07-12 15:46:15 +0100

    Working check for tf names using underscores.

--- sample-terraform/main.tf
+++ sample-terraform/main.tf
@@ -4,3 +4,15 @@
 resource "aws_iam" "test" {
   name = "Test"
 }
+
+resource "something" "invalid-name" {
+  name = "test"
+}
+
+module "some-module" {
+  // Test
+  source            = "./modules/kinesis"
+  stream_name       = "institution_${var.jisc_id}_input_${terraform.env}"
+  shard_count       = "${var.kinesis_shard_count}"
+  retention_period  = "${var.kinesis_retention_period}"
+}
--- terrachecker.py
+++ terrachecker.py
@@ -12,9 +12,9 @@ class TerraformChecker(object):
     def __init__(self, dir_path):
         """Read the Terraform from the provided path."""
         self.root_dir_path = dir_path
-        self.walks = self._get_walks(dir_path)
+        self.walks = self._get_path_walks(dir_path)
 
-    def _get_walks(self, dir_path):
+    def _get_path_walks(self, dir_path):
         """Finds all Terraform configs in the given dir path."""
         ret = []
         for root, subdirs, files in os.walk(dir_path):
@@ -41,7 +41,9 @@ class TerraformChecker(object):
 
     def _check_file(self, file_path):
         """Loop through and validates list of files."""
-        self.errors.extend(self._root_tf_file_errors(file_path))
+
+        if file_path.endswith('.tf'):
+            self.errors.extend(self._root_tf_file_errors(file_path))
 
     def _root_tf_file_errors(self, path):
         """Check the root terraform and return a list of errors or None."""
@@ -52,14 +54,30 @@ class TerraformChecker(object):
             print("Checking file at path: {}".format(path))
             try:
                 obj = hcl.load(f)
+                ret.append(self._hcl_tf_errors(path, obj))
             except ValueError as ve:
                 ret.append(self._error_dict(path, str(ve)))
 
         return []
 
-    def _check_module(self, file_path):
-        """Checks a specific module."""
-        pass
+    def _hcl_tf_errors(self, path, hcl_obj):
+        """Check a givn hcl object and return any validation errors."""
+        ret = []
+        if 'module' in hcl_obj:
+            ret.append(self._tf_module_errors(path, hcl_obj['module']))
+
+        return ret
+
+    def _tf_names_not_using_underscore_errors(self, path, modules):
+        """Find any errors in the provided module."""
+        ret = []
+        for k, v in modules.items():
+            if '-' in k:
+                ret.append(self._error_dict(
+                    path,
+                    'Module "{}" contains a "-", use "_" instead.'.format(k)))
+
+        return ret
 
     def _error_dict(self, path, error):
         """Adds an error to the errors array."""
