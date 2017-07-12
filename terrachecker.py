21cf5f31e44d585b35d71cb9d706b90b0ca14a72
Mark Winterbottom <mark@londonappdeveloper.com>
2017-07-12 15:16:59 +0100

    Validate that the file contains HCL.

--- terrachecker.py
+++ terrachecker.py
@@ -5,7 +5,7 @@ import hcl
 class TerraformChecker(object):
     """Terraform checking class."""
 
-    root_dir_path = ''
+    root_dir_path = None
     walks = []
     errors = []
 
@@ -27,35 +27,35 @@ class TerraformChecker(object):
 
     def validate(self):
         """Checks the Terraform configs."""
-        import pdb; pdb.set_trace()
         for walk in self.walks:
-            root = walk['root']
+            root = self._ensure_trailing_slash(walk['root'])
             subdirs = walk['subdirs']
             files = walk['files']
 
             for file_name in files:
                 self._check_file(root + file_name)
 
+    def _ensure_trailing_slash(self, root):
+        """Ensures the given path has a trailing slash."""
+        return root if root.endswith('/') else root + '/'
+
     def _check_file(self, file_path):
         """Loop through and validates list of files."""
-
         self.errors.extend(self._root_tf_file_errors(file_path))
 
     def _root_tf_file_errors(self, path):
         """Check the root terraform and return a list of errors or None."""
-
         ret = []
 
         # Check module names do not contain resource with hyphen.
         with open(path) as f:
-            obj = hcl.load(f)
-            import pdb; pdb.set_trace()
+            print("Checking file at path: {}".format(path))
+            try:
+                obj = hcl.load(f)
+            except ValueError as ve:
+                ret.append(self._error_dict(path, str(ve)))
 
         return []
-
-    def _check_file(self, file_path):
-        """Checks a specific file path."""
-        pass
 
     def _check_module(self, file_path):
         """Checks a specific module."""
