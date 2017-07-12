c7ad089bd6be10824801baf236d62cced6ed3c56
Mark Winterbottom <mark@londonappdeveloper.com>
2017-07-12 16:47:23 +0100

    Working validation on terraform file names and module names.

--- terrachecker.py
+++ terrachecker.py
@@ -5,85 +5,79 @@ import hcl
 class TerraformChecker(object):
     """Terraform checking class."""
 
-    root_dir_path = None
-    walks = []
-    errors = []
+    tf_file_paths = []
+    validation_errors = []
 
-    def __init__(self, dir_path):
+    def __init__(self, root_path):
         """Read the Terraform from the provided path."""
-        self.root_dir_path = dir_path
-        self.walks = self._get_path_walks(dir_path)
+        self.validation_errors = []
+        self.tf_file_paths = self._get_tf_file_paths(root_path)
 
-    def _get_path_walks(self, dir_path):
-        """Finds all Terraform configs in the given dir path."""
+    def _get_tf_file_paths(self, root_path):
+        """List paths for all the Terraform files to check."""
         ret = []
-        for root, subdirs, files in os.walk(dir_path):
-            ret.append({
-                'root': root,
-                'subdirs': subdirs,
-                'files': files
-            })
+        for root, subdirs, files in os.walk(root_path):
+            for file_name in files:
+                if file_name.endswith('.tf'):
+                    root_path = self._ensure_trailing_slash(root)
+                    full_path = root_path + file_name
+                    ret.append(full_path)
+
         return ret
 
     def validate(self):
         """Checks the Terraform configs."""
-        for walk in self.walks:
-            root = self._ensure_trailing_slash(walk['root'])
-            subdirs = walk['subdirs']
-            files = walk['files']
 
-            for file_name in files:
-                self._check_file(root + file_name)
+        for file_path in self.tf_file_paths:
+            self._validate_tf_file(file_path)
+            self._validate_tf_file_name(file_path)
+
+        return self.validation_errors
+
+    def _validate_tf_file_name(self, file_path):
+        """Check tf file name is either main.tf, output.tf or variables.tf."""
+
+        file_name = file_path.split('/')[-1]
+
+        if file_name not in ['main.tf', 'variables.tf', 'outputs.tf']:
+            self._add_error(
+                file_path,
+                'Name {} is invalid. Please use only '
+                '"main.tf", "variables.tf" or "outputs.tf"'.format(
+                    file_name, file_path))
+
+    def _validate_tf_file(self, file_path):
+        """Load Terraform file at given path and validate."""
+
+        with open(file_path) as f:
+            try:
+                obj = hcl.load(f)
+            except ValueError as ve:
+                self._add_error(file_path, str(ve))
+                return
+
+            self._validate_tf_section_names(file_path, obj)
+
+    def _validate_tf_section_names(self, file_path, hcl_obj):
+        """Check that tf module, resource, variable, etc... names are valid."""
+        for section, v in hcl_obj.items():
+            for name, v2 in hcl_obj[section].items():
+                if '-' in name:
+                    self._add_error(
+                        file_path,
+                        '{} "{}" contains a "-" in the name. Use '
+                        '"_" instead.'.format(section.capitalize(), name)
+                    )
 
     def _ensure_trailing_slash(self, root):
         """Ensures the given path has a trailing slash."""
         return root if root.endswith('/') else root + '/'
 
-    def _check_file(self, file_path):
-        """Loop through and validates list of files."""
-
-        if file_path.endswith('.tf'):
-            self.errors.extend(self._root_tf_file_errors(file_path))
-
-    def _root_tf_file_errors(self, path):
-        """Check the root terraform and return a list of errors or None."""
-        ret = []
-
-        # Check module names do not contain resource with hyphen.
-        with open(path) as f:
-            print("Checking file at path: {}".format(path))
-            try:
-                obj = hcl.load(f)
-                ret.append(self._hcl_tf_errors(path, obj))
-            except ValueError as ve:
-                ret.append(self._error_dict(path, str(ve)))
-
-        return []
-
-    def _hcl_tf_errors(self, path, hcl_obj):
-        """Check a givn hcl object and return any validation errors."""
-        ret = []
-        if 'module' in hcl_obj:
-            ret.append(self._tf_module_errors(path, hcl_obj['module']))
-
-        return ret
-
-    def _tf_names_not_using_underscore_errors(self, path, modules):
-        """Find any errors in the provided module."""
-        ret = []
-        for k, v in modules.items():
-            if '-' in k:
-                ret.append(self._error_dict(
-                    path,
-                    'Module "{}" contains a "-", use "_" instead.'.format(k)))
-
-        return ret
-
-    def _error_dict(self, path, error):
-        """Adds an error to the errors array."""
-        self.errors.append({'path': path, 'error': error})
+    def _add_error(self, path, error):
+        """Adds an error to the validation errors."""
+        self.validation_errors.append({'path': path, 'error': error})
 
 
 checker = TerraformChecker('sample-terraform/')
 checker.validate()
-print(checker.errors)
+print(checker.validation_errors)
